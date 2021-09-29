a932a1035e8622768211662ad481bfc891794fe1
Guilherme Henrique <guiihenriquecs@gmail.com>
2021-09-29 01:04:54 -0300

    Fixing Bugs in JS

--- src/js/script.js
+++ src/js/script.js
@@ -1,48 +1,39 @@
 const calculate = document.getElementById('calculate');
 
-const name = document.getElementById('namePacient');
-const weight = document.getElementById('weightPacient');
-const height = document.getElementById('heightPacient');
+let name   = document.getElementById('namePacient').value;
+let weight = document.getElementById('weightPacient');
+let height = document.getElementById('heightPacient');
 
-const result = document.getElementById('result');
-const inputs = document.querySelectorAll(".input");
+let result = document.getElementById('result');
+let inputs = document.querySelectorAll(".input");
+
 
 const focusFunx = () => {
   let parent = this.parentNode.parentNode;
   parent.classList.add("focus");
 };
 
-const calculateIMC = () => {
-  if(namePacient !== '' && weightPacient.value !== '' && heightPacient.value !== ''){
-    const resultIMC = (parseFloat(weightPacient.value) / parseFloat(heightPacient.value ** 2) ).toFixed(1)
+let calculateIMC = () => {
+  if(namePacient.value !== '' && weightPacient.value !== '' && heightPacient.value !== ''){
+    let resultIMC = (parseFloat(weightPacient.value) / parseFloat(heightPacient.value ** 2)).toFixed(1)
     if(resultIMC < 18.5 ){
-      const messageReturn = "magreza"
-      result.textContent = "Seu IMC é" + resultIMC
-      result.textContent = "Você está em situação de " + messageReturn
+      const messageReturn = "Magreza"
+      result.textContent = `${namePacient.value} seu IMC é de  ${resultIMC}  e você está em situação de  ${messageReturn} Procure um médico para receber ajuda!` 
   }else if (resultIMC >= 18.5 && resultIMC <= 24.9){
       const messageReturn = "Peso Normal"
-      result.textContent = "Seu IMC é" + resultIMC
-      result.textContent = "Você está com o seu " + messageReturn
+      result.textContent = `${namePacient.value} seu IMC é de  ${resultIMC}  e você está com seu  ${messageReturn} Procure um médico para receber ajuda!` 
   } else if (resultIMC >= 25.0 && resultIMC <= 29.9){
       const messageReturn = "Acima do peso"
-      result.textContent = "Seu IMC é" + resultIMC
-      result.textContent = "Você está " + messageReturn
-      result.textContent = "Consulte um médico mais proximo para receber mais orientaçoes"
+      result.textContent = `${namePacient.value} seu IMC é de " ${resultIMC}  e você está  ${messageReturn} Procure um médico para receber mais orientaçoes!` 
   } else if(resultIMC >= 30.0  && resultIMC <= 34.9){
       const messageReturn = "Obesidade Grau I"
-      result.textContent = "Seu IMC é" + resultIMC
-      result.textContent = "Você está diagnosticada com " + messageReturn
-      result.textContent = "Consulte um médico urgente!" 
+      result.textContent = `${namePacient.value} seu IMC é de  ${resultIMC}  e você está com  ${messageReturn} Procure um médico para receber mais orientaçoes!`
   } else if(resultIMC >= 35.0  && resultIMC <= 39.9){
       const messageReturn ="Obesidade Grau II"
-      result.textContent = "Seu IMC é" + resultIMC
-      result.textContent = "Você está diagnosticada com " + messageReturn
-      result.textContent = "Consulte um médico urgente!" 
+      result.textContent = `${namePacient.value} seu IMC é de  ${resultIMC}  e você está com  ${messageReturn} Procure um médico para receber mais orientaçoes!`
   } else {
       const messageReturn = "Obesidade Grau III"
-      result.textContent = "Seu IMC é" + resultIMC
-      result.textContent = "Você está diagnosticada com " + messageReturn
-      result.textContent = "Consulte um médico urgente!" 
+      result.textContent = `${namePacient} seu IMC é de  ${resultIMC}  e você está com  ${messageReturn} Procure um médico urgentemente para receber mais orientaçoes!`
   }
   }else{
     alert('Preencha todos os campos para que o calculo possa ser feito.')
