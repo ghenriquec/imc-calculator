863b41e602ae8f84177461a9e2e4e892030b3187
Guilherme Henrique <guiihenriquecs@gmail.com>
2021-10-12 20:22:28 -0300

    v1.7

--- src/js/script.js
+++ src/js/script.js
@@ -1,27 +1,25 @@
 const calculate = document.getElementById('calculate');
 
-const name   = document.getElementById('namePacient').value;
-const weight = document.getElementById('weightPacient');
-const height = document.getElementById('heightPacient');
-
-let result = document.getElementById('result');
 let inputs = document.querySelectorAll(".input");
 
 /* Mask for input */
-$(document).ready(function() {
-  $('.weight').mask("#00.0", {reverse: true});
-}) 
+$(document).ready(function () {
+  $('.weight').mask("#00", {
+    reverse: true
+  });
+})
 
-$(document).ready(function() {
-  $('.height').mask("#0.00", {reverse: true});
-}) 
-
+$(document).ready(function () {
+  $('.height').mask("#0.00", {
+    reverse: true
+  });
+})
 
 const clearText = () => {
   document.getElementById('namePacient').value = "";
   document.getElementById('weightPacient').value = "";
   document.getElementById('heightPacient').value = "";
-  document.getElementById('result').value = "";
+  document.getElementById('result').innerHTML = "";
 }
 
 const focusFunx = () => {
@@ -29,35 +27,41 @@ const focusFunx = () => {
   parent.classList.add("focus");
 };
 
-let calculateIMC = () => {
-  if(namePacient.value !== '' && weightPacient.value !== '' && heightPacient.value !== ''){
-    let resultIMC = (parseFloat(weightPacient.value) / parseFloat(heightPacient.value ** 2)).toFixed(1)
-    if(resultIMC < 18.5 ){
-      const messageReturn = "magreza"
-      result.textContent = `${namePacient.value} seu IMC é de  ${resultIMC} e você está em situação de ${messageReturn}. Procure um médico para receber mais orientaçoes!`
-  }else if (resultIMC >= 18.5 && resultIMC <= 24.9){
-      const messageReturn = "peso ideal"
-      result.textContent = `${namePacient.value} seu IMC é de  ${resultIMC}  e você está com seu ${messageReturn}` 
-  } else if (resultIMC >= 25.0 && resultIMC <= 29.9){
-      const messageReturn = "acima do peso"
-      result.textContent = `${namePacient.value} seu IMC é de ${resultIMC}  e você está  ${messageReturn} Procure um médico para receber mais orientaçoes!` 
-  } else if(resultIMC >= 30.0  && resultIMC <= 34.9){
-      const messageReturn = "obesidade grau I"
-      result.textContent = `${namePacient.value} seu IMC é de  ${resultIMC}  e você está com  ${messageReturn} Procure um médico para receber mais orientaçoes!`
-  } else if(resultIMC >= 35.0  && resultIMC <= 39.9){
-      const messageReturn ="obesidade grau II"
-      result.textContent = `${namePacient.value} seu IMC é de  ${resultIMC}  e você está com  ${messageReturn} Procure um médico para receber mais orientaçoes!`
+const calculateIMC = () => {
+  const namePacient = document.getElementById('namePacient');
+  const weightPacient = document.getElementById('weightPacient');
+  const heightPacient = document.getElementById('heightPacient');
+  const result = document.getElementById('result');
+
+  if (namePacient.value !== '' && weightPacient.value !== '' && heightPacient.value !== '') {
+    const resultIMC = (parseFloat(weightPacient.value) / parseFloat(heightPacient.value ** 2)).toFixed(2)
+    const weightMaximum = (18.5 * (heightPacient.value ** 2)).toFixed(1);
+    const weightMinimum = (25 * (heightPacient.value ** 2)).toFixed(1);
+
+    let messageReturn = '';
+
+    if (resultIMC < 18.5) {
+      messageReturn = 'abaixo do peso.';
+    } else if (resultIMC < 25) {
+      messageReturn = 'com o peso ideal';
+    } else if (resultIMC < 30) {
+      messageReturn = 'acima do peso.';
+    } else if (resultIMC < 35) {
+      messageReturn = 'com obesidade de grau I.';
+    } else if (resultIMC < 40) {
+      messageReturn = 'com obesidade de grau II.';
+    } else {
+      messageReturn = 'com obesidade de grau III.';
+    }
+    result.textContent = `${namePacient.value} seu imc é de ${resultIMC} e você está ${messageReturn}. Procure manter seu peso entre ${weightMinimum} Kg & ${weightMaximum} Kg`
   } else {
-      const messageReturn = "obesidade grau III"
-      result.textContent = `${namePacient} seu IMC é de  ${resultIMC}  e você está com  ${messageReturn} Procure um médico urgentemente para receber mais orientaçoes!`
-  }
-  }else{
-    alert('Preencha todos os campos para que o calculo possa ser feito.')
+    alert('Para calcular o seu IMC , preencha todos os campos.');
   }
 }
+
+calculate.addEventListener("click", calculateIMC)
 
 inputs.forEach((input) => {
   input.addEventListener("focus", focusFunx);
 });
 
-calculate.addEventListener("click", calculateIMC)
